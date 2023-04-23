743f98287c6d6baeaa2f3534ea2c3cd663b5aa38
xpertdreamer <106870568+xpertdreamer@users.noreply.github.com>
2023-04-23 21:23:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ import binatytree.BinaryTree;
 public class Main {
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
-        tree.add(1);
-        tree.add(2);
-        tree.add(3);
         tree.add(4);
-        tree.add(5);
+        tree.add(2);
         tree.add(6);
+        tree.add(1);
+        tree.add(5);
+        tree.add(3);
         tree.add(7);
         tree.add(8);
         tree.add(9);
-        tree.add(10);
-        tree.add(11);
         tree.add(12);
+        tree.add(11);
+        tree.add(10);
         tree.add(13);
         System.out.println(tree.containsNode(10));
 
